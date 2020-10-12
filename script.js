702652bf305759c7d11cc9eab5c9a9d424a4c9be
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-10-12 16:48:12 -0700

    click functions for sidebar nav

---
+++ script.js
@@ -0,0 +1,17 @@
+(function($) {
+    "use sctrict";
+
+    $('.menu-toggle').click(function(e) {
+        e.preventDefault();
+        $('#sidebar-wrapper').toggleClass("active");
+        $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+        $(this).toggleClass("active");
+    });
+
+    $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+        $("#sidebar-wrapper").removeClass("active");
+        $(".menu-toggle").removeClass("active");
+        $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    });
+    
+})(jQuery);
